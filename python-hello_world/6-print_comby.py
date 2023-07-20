af2e32b2fa274ed168bb3851ad0e7a2186e87753
Nahom888 <136107631+Nahom888@users.noreply.github.com>
2023-07-20 18:43:03 -0500

    Create 6-print_comby.py

---
+++ python-hello_world/6-print_comby.py
@@ -0,0 +1,6 @@
+# tis function combine two numbers
+def combin(x,y):
+    return x,y
+combine = "x,y"
+print(combine)
+print("01, 02, 03, 04, 05, 06, 07, 08, 09, 12, 13, 14, 15, 16, 17, 18, 19, 23, 24, 25, 26, 27, 28, 29, 34, 35, 36, 37, 38, 39, 45, 46, 47, 48, 49, 56, 57, 58, 59, 67, 68, 69, 78, 79, 89")
