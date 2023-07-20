dad951c189d3cbd190eb6e7c9edb7a59608bd802
Nahom888 <136107631+Nahom888@users.noreply.github.com>
2023-07-20 16:36:12 -0500

    Update 2-positive_or_negative.py

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,9 +1,10 @@
-number = any(-10,10)
-print("input number")
-print(number)
-if number>0 print("posetive")
-if number=0 print("zero")
-if number<0 print("negative")
+number = any(-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10)
+if number > 0 
+    print("posetive")
+if number = 0
+    print("zero")
+if number < 0
+    print("negative")
 
 while True
     # take input from the user
@@ -16,4 +17,4 @@ while True
                            
 
     else:
-        print("invalid input")                                                      )               
+        print("wrong type")                                                      )               
