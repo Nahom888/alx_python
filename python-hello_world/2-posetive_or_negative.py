32f3e6c56270e2976df1929cfd68f1534f594a95
Nahom888 <136107631+Nahom888@users.noreply.github.com>
2023-07-20 15:39:03 -0500

    Create 2-posetive_or_negative.py

---
+++ python-hello_world/2-posetive_or_negative.py
@@ -0,0 +1,19 @@
+number = any(-10,10)
+print("input number")
+print(number)
+if number>0 print("posetive")
+if number=0 print("zero")
+if number<0 print("negative")
+
+while True
+    # take input from the user
+    choice = input("enter choice(-10,10)")
+          
+    # check if choice is one of the two option
+    if choice in (-10,0):
+        try:
+            number = float(input("enter the number")
+                           
+
+    else:
+        print("invalid input")                                                      )               
