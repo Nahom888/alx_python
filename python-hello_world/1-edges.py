a044c851941e3e9b5d831296e7391b25dd392716
Nahom888 <136107631+Nahom888@users.noreply.github.com>
2023-07-20 16:19:45 -0500

    Create 1-edges.py

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,8 @@
+word = "Holberton"
+print( word )
+print("first 3 words")
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
+
+else print("invalid")
