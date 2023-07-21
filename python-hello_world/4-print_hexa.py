49bb16fb40e90b9f4e2f4e42da68541668ec0d36
Nahom888 <136107631+Nahom888@users.noreply.github.com>
2023-07-20 19:05:56 -0500

    Create 4-print_hexa.py

---
+++ python-hello_world/4-print_hexa.py
@@ -0,0 +1,11 @@
+#This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+try:
+            num1 = ("0"))
+            num2 = float(input("Enter second number: "))
+   except ValueError:
+        elif 
+             print(num1, "*", num2, "=", multiply(num1, num2))
+            
+    
